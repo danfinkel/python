b60577030cde9eec46d16f076158db8e5025322d
dfinkel <dan.finkel@gmail.com>
2018-08-12 15:10:28 -0400

    updated resume

--- danfinkel.github.io/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin15/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/dfinkel/git_base/danfinkel.github.io/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
--- danfinkel.github.io/vendor/bundle/gems/rouge-3.2.0/lib/rouge/version.rb
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*- #
-
-module Rouge
-  def self.version
-    "3.2.0"
-  end
-end
